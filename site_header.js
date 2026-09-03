d338f88819d8517e330c20c7dccbfc08eb112d9d
James Bunker <jamesxbunker@gmail.com>
2026-09-02 22:09:18 -0400

    Added post loader to posts.html, now it will load in posts dynamically from the posts folder. It grabs meta data from these posts to create a preview for each one. Updated how site header makes active tabs, now if a path contains post it will be active (may be problematic in the future but starting here). Updated blog template and added style sheet for it.

--- site_header.js
+++ site_header.js
@@ -11,11 +11,11 @@ class SiteHeader extends HTMLElement {
         <span>Geosmin</span>
       </a>
       <ul class="nav-tabs">
-        <li><a href="index.html" class="${currentPath === 'index.html' ? 'active' : ''}"> <span>Home</span></a></li>
-        <li><a href="posts.html" class="${currentPath === 'posts.html' ? 'active' : ''}"> <span>Posts</span></a></li>
-        <li><a href="projects.html" class="${currentPath === 'projects.html' ? 'active' : ''}"> <span>Projects</span></a></li>
-        <li><a href="about.html" class="${currentPath === 'about.html' ? 'active' : ''}"> <span>About</span></a></li>
-        <li><a href="sensor.html" class="${currentPath === 'sensor.html' ? 'active' : ''}"> <span>Sensor</span></a></li>
+        <li><a href="/index.html" class="${currentPath === 'index.html' ? 'active' : ''}"> <span>Home</span></a></li>
+        <li><a href="/posts.html" class="${window.location.pathname.includes('post') ? 'active' : ''}"> <span>Posts</span></a></li>
+        <li><a href="/projects.html" class="${currentPath === 'projects.html' ? 'active' : ''}"> <span>Projects</span></a></li>
+        <li><a href="/about.html" class="${currentPath === 'about.html' ? 'active' : ''}"> <span>About</span></a></li>
+        <li><a href="/sensor.html" class="${currentPath === 'sensor.html' ? 'active' : ''}"> <span>Sensor</span></a></li>
       </ul>
     </div>
   </header>
